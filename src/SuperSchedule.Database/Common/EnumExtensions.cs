247d9358ef7abd6e7253e28127563ebe4879078a
yoannakr <jkrmilkova@gmail.com>
2022-06-20 16:32:14 +0300

    Changed location of enum extensions.

--- src/SuperSchedule.Database/Common/EnumExtensions.cs
+++ src/SuperSchedule.Database/Common/EnumExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
-namespace SuperSchedule.Database.Enums
+namespace SuperSchedule.Database.Common
 {
     public static class EnumExtensions
     {
