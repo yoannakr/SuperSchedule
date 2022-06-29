dbcd7e91fa4ceadfd0b56fb808fbb95f6b7502f7
yoannakr <jkrmilkova@gmail.com>
2022-06-29 08:38:20 +0300

    Added installer for 32-bit windows.

--- src/SuperSchedule.UI/desktop/public/server.js
+++ src/SuperSchedule.UI/desktop/public/server.js
@@ -1,18 +1,23 @@
 const path = require("path");
+const arch = process.env.PROCESSOR_ARCHITECTURE;
 
 function startServer() {
   const nodeConsole = require("console");
   const myConsole = new nodeConsole.Console(process.stdout, process.stderr);
   myConsole.log("Server connecting......");
 
-  // Change working directory
-  //process.chdir("../../");
-
   let backend;
-  backend = path.join(
-    __dirname,
-    "../build/api/net6.0/publish/SuperSchedule.Startup.exe"
-  );
+  if (arch === "AMD64") {
+    backend = path.join(
+      __dirname,
+      "../build/api/net6.0/publish/win64/SuperSchedule.Startup.exe"
+    );
+  } else {
+    backend = path.join(
+      __dirname,
+      "../build/api/net6.0/publish/win32/SuperSchedule.Startup.exe"
+    );
+  }
 
   var execfile = require("child_process").execFile;
   execfile(
